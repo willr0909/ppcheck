fbeab2fc38d322694b00fe5b2431184754677487
willr0909 <77408953+willr0909@users.noreply.github.com>
2021-01-13 12:41:20 -0600

    Add files via upload

---
+++ project/project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project
+//
+//  Created by P.M. Student on 1/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ project/project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by P.M. Student on 1/13/21.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
